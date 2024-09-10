3cb3b819457a4182d810ac4d33986a517782ef4d
nobeljb <nobeljulianbintang@gmail.com>
2024-09-10 10:32:48 +0700

    adding boat

--- main/views.py
+++ main/views.py
@@ -4,7 +4,9 @@ def show_main(request):
     context = {
         'npm' : '2306202826',
         'name': 'Nobel',
-        'class': 'PBP F'
+        'class': 'PBP F',
+        'boat' : 'Budiono Siregar',
+        'yearmanufacture' : '2005'
     }
 
     return render(request, "main.html", context)
